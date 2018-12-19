b2ce92ce565ea71842999638eb568d45cb75c1fb
uoon-dev <uoon.dev@gmail.com>
2018-12-20 02:21:59 +0900

    add SideDrawer component

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import { NotificationContainer } from 'react-notifications';
 import Alert from 'react-s-alert';
 import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <>
-    <Toolbar />
-    <main>
-      {props.children}
-    </main>
-    <Alert stack={{ limit: 3 }} />
-    <NotificationContainer />
-  </>
-)
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler} />
+        <main>
+          {this.props.children}
+        </main>
+        <Alert stack={{ limit: 3 }} />
+        <NotificationContainer />
+      </>
+    )
+  }
+}
+
+export default Layout;
